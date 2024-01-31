3c867b119bcab1214d4dc80a24ac838d9f26fd3c
RGLENNO <Richard_Glennon@glic.com>
2024-01-31 15:16:44 -0500

    added space

--- test_bank.py
+++ test_bank.py
@@ -6,7 +6,7 @@ def test_view_balance_success():
     bank_account = BankAccount(username='name', balance=100)
     actual_balance = bank_account.view_balance()
     assert actual_balance == 100
-
+ 
 def test_view_balance_unsuccessful():
     bank_account = BankAccount(username='name', balance=-100)
     with pytest.raises(BalanceRetrievalException):
