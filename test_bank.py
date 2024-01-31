17357ac730ea2b2abd6cfd94f0c569f4148226f4
TudorMironovici <tmironovici@gmail.com>
2024-01-31 15:04:54 -0500

    initial change

--- test_bank.py
+++ test_bank.py
@@ -11,3 +11,14 @@ def test_view_balance_unsuccessful():
     bank_account = BankAccount(username='name', balance=-100)
     with pytest.raises(BalanceRetrievalException):
         bank_account.view_balance()
+
+def test_deposit_success():
+    bank_account = BankAccount(username='name', balance=100)
+    deposit_balance = bank_account.deposit(60)
+    assert deposit_balance == 160
+    
+def test_deposit_unsuccess():
+    bank_account = BankAccount(username='name', balance=100)
+    deposit_balance = bank_account.view_balance('a')
+    with pytest.raises(InternalDepositException):
+        bank_account.view_balance()
